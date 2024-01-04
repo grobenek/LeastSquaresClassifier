cbd7e1b35c2ff91ee2ea83067930b5b46687da0b
Peter Szathmary <peter.szathmary01@gmail.com>
2024-01-04 20:22:54 +0100

    Finish

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import os
 
 import numpy as np
 from PIL import Image
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 from sklearn.preprocessing import LabelBinarizer
 
 
-def load_data_from_folders(base_path):
+def load_data(path):
     images = []
     labels = []
 
-    for label in os.listdir(base_path):
-        folder_path = os.path.join(base_path, label)
+    for label in os.listdir(path):
+        folder_path = os.path.join(path, label)
         if os.path.isdir(folder_path):
             for image_name in os.listdir(folder_path):
                 image_path = os.path.join(folder_path, image_name)
                 image = Image.open(image_path)
-                images.append(np.array(image).astype('float32'))
+                images.append(np.array(image).astype('float64'))
                 labels.append(int(label))
 
     images = np.array(images).reshape(-1, 28 * 28)
@@ -28,29 +28,30 @@ train_path = 'dáta/train'
 test_path = 'dáta/test'
 
 # Load the data
-train_images, train_labels = load_data_from_folders(train_path)
-test_images, test_labels = load_data_from_folders(test_path)
+images_train, labels_train = load_data(train_path)
+images_test, labels_test = load_data(test_path)
 
 # Encoding data
 encoder = LabelBinarizer(sparse_output=False)
-train_labels_one_hot = encoder.fit_transform(train_labels.reshape(-1, 1))
+train_labels_one_hot = encoder.fit_transform(labels_train.reshape(-1, 1))
 
-test_labels_one_hot = encoder.transform(test_labels.reshape(-1, 1))
+test_labels_one_hot = encoder.transform(labels_test.reshape(-1, 1))
 
-# Compute the weight matrix W
-X = train_images
+# weights -> 𝛽=(𝐴𝑇𝐴)^-1 * 𝐴𝑇𝑌
+X = images_train
 Y = train_labels_one_hot
-W = np.linalg.pinv(X.T @ X) @ X.T @ Y
+inverse_matrix = np.linalg.pinv(X.T @ X)
+W = inverse_matrix @ X.T @ Y
 
 
-def predict(images, W):
+def predict_data(images, W):
     return np.argmax(images @ W, axis=1)
 
 
 # evaluation
-test_predictions = predict(test_images, W)
+test_predictions = predict_data(images_test, W)
 
-confusion_matrix = confusion_matrix(test_labels, test_predictions)
+confusion_matrix = confusion_matrix(labels_test, test_predictions)
 
 
 def print_confusion_matrix(p_confusion_matrix):
@@ -63,17 +64,17 @@ def print_confusion_matrix(p_confusion_matrix):
 print_confusion_matrix(confusion_matrix)
 
 # accuracy
-accuracy = accuracy_score(test_labels, test_predictions)
-print(f"Accuracy: {accuracy}")
+accuracy = accuracy_score(labels_test, test_predictions)
+print(f"Accuracy: {accuracy * 100}%")
 
 # precision
-precision = precision_score(test_labels, test_predictions, average='weighted')
-print(f"Precision: {precision}")
+precision = precision_score(labels_test, test_predictions, average='weighted')
+print(f"Precision: {precision * 100}%")
 
 # recall
-recall = recall_score(test_labels, test_predictions, average='weighted')
-print(f"Recall: {recall}")
+recall = recall_score(labels_test, test_predictions, average='weighted')
+print(f"Recall: {recall * 100}%")
 
-# F1 score
-f1 = f1_score(test_labels, test_predictions, average='weighted')
-print(f"F1 Score: {f1}")
+# error rate
+error_rate = 100 - (accuracy * 100)
+print(f'Error rate: {error_rate}%')
